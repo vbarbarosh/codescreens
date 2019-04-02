6a8ea361a9ed21d5681e0b5897fc67ddad85b103
Vladimir Barbarosh <vladimir.barbarosh@gmail.com>
2019-04-02 21:48:01 +0300

    reword

--- demos/js_regex_escape.js
+++ demos/js_regex_escape.js
@@ -1,4 +1,4 @@
-// Make a string safe to put into RegExp
+// Make a string safe to put into regular expression
 // https://stackoverflow.com/a/6969486
 // https://stackoverflow.com/a/3561711
 function js_escape_regexp(s)
