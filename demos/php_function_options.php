26b74132772aa4b0a03000c3b671528faeea0d33
Vladimir Barbarosh <vladimir.barbarosh@gmail.com>
2019-04-08 21:18:44 +0300

    add php_function_options

---
+++ demos/php_function_options.php
@@ -0,0 +1,22 @@
+<?php
+
+# Default options in functions
+
+function foobar($opt = [])
+{
+    $opt += [
+		'name' => 'default_name',
+		'title' => 'Default Title',
+    ];
+
+	echo 'name: ', $opt['name'], PHP_EOL;
+	echo 'title: ', $opt['title'], PHP_EOL;
+}
+
+# name: default_name
+# title: Default Title
+foobar();
+
+# name: custom_name
+# title: Default title
+foobar(['name' => 'custom_name']);
